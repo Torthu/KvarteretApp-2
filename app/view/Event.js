7d5f71c58e4bfbcfe924c641f7db33363630a3ba
Torstein Thune <torstein@gmail.com>
2012-08-29 11:01:05 +0200

    Changed template for single events, now looks somewhat similar to the sketch

---
+++ app/view/Event.js
@@ -0,0 +1,37 @@
+Ext.define("KvarteretApp.view.Event", {
+    extend: 'Ext.Panel',
+    requires: ['Ext.Toolbar', 'Ext.Button'],
+    xtype: 'event',
+
+    config: {
+        Title: 'Event',
+        xtype: 'event',
+        layout: 'vbox',
+        id: 'event',
+        fullscreen: 'true',
+
+        scrollable: 'vertical',
+
+        tpl: [
+        	'<div class="singleEvent wrapper">',
+			'	<div class="clearfix imageWrapper">',
+					'<img src="{primaryPictureUrl}" />',
+					'<h1 class="text_center">{title}</h1>',
+					'<small class="meta text_center">{startDateString} kl. {startTimeShort} - {endDateString}{endTimeShort}</small>',
+				'</div>',
+				'<div class="left half text_center meta">CC: {covercharge}</div>',
+				'<div class="right half text_center meta">Age: {age_limit}</div>',
+				'<div class="lead">{leadParagraph}</div>',
+				'<div class="description">{description}</div>',
+			'</div>'
+		],
+		items: [
+			{	
+				xtype:'toolbar',
+				title: "KvarteretApp",
+				docked:'top',
+				items: [ {xtype:'button', ui:'back',text:'back', cls:'backButton', action:"eventBack"} ],
+			}
+		]
+    }
+});
