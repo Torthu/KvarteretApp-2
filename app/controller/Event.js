a8d45e2faa09a4b95b58a93c62cd104a91120e64
Torstein Thune <torstein@gmail.com>
2012-09-05 13:49:11 +0200

    edited to use new stack functions

--- app/controller/Event.js
+++ app/controller/Event.js
@@ -10,31 +10,41 @@ Ext.define('KvarteretApp.controller.Event', {
 
          this.control({
             /* events */
-            '#eventList':{
+            '.eventList': {
                 select: function (selectedItem) {
 
                     /*  creates a prototype view using Event view
                         sends in data from selected element which is handled by the views tpl
                     */
                     var event = Ext.create('KvarteretApp.view.Event', {
-                        data: selectedItem.getSelection()[0].data
+                        data: selectedItem.getSelection()[0].data,
+                        id: 'event'
                     });
 
-                    eventWrapper = Ext.getCmp('eventWrapper');
-                    eventWrapper.add(event);
-                    eventWrapper.setActiveItem(event);
+                    // eventWrapper = Ext.getCmp('eventWrapper');
+                    // eventWrapper.add(event);
+                    // eventWrapper.setActiveItem(event);
+
+                    // Ext.Viewport.add(event);
+                                       
+                    // make it active
+                    // Ext.Viewport.setActiveItem(event);
+                    stackForward(event);
 
                     eventList = Ext.getCmp('eventList');
                     eventList.deselectAll();
                 }
             },
-            
+
             'button[action=eventBack]':{
                 tap: function (selectedItem) {
-                    console.log('going back');
                     
-                    eventWrapper.remove(Ext.getCmp('event'));
-                    eventWrapper.setActiveItem(eventList);
+                    // eventWrapper.remove(Ext.getCmp('event'));
+                    // eventWrapper.setActiveItem(eventList);
+                    // Ext.Viewport.remove(Ext.getCmp('event'));
+                    // Ext.Viewport.setActiveItem(Ext.getCmp('main'));
+
+                    stackBack();
 
                 }
             }
